c11bd454d221d9738a53812315eab4dadd614a65
Aditya Mohite <adityamohite73@gmail.com>
2024-12-02 23:40:36 +0530

    Adding simple arithematic functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int divide(int a, int b) {
+    return a / b;
+}
+
+int modulo(int a, int b) {
+    return a % b;
+}
+
 int main() {
     int num1;
     int num2;
@@ -7,11 +27,12 @@ int main() {
     std::cout << "Enter two numbers: ";
     std::cin >> num1 >> num2;
 
-    std::cout << "The sum of " << num1 << " and " << num2 << " is " << num1 + num2 << std::endl;
-    std::cout << "The difference of " << num1 << " and " << num2 << " is " << num1 - num2 << std::endl;
-    std::cout << "The product of " << num1 << " and " << num2 << " is " << num1 * num2 << std::endl;
-    std::cout << "The quotient of " << num1 << " and " << num2 << " is " << num1 / num2 << std::endl;
-    std::cout << "The remainder of " << num1 << " and " << num2 << " is " << num1 % num2 << std::endl;
+    // call functions
+    std::cout << "The sum of " << num1 << " and " << num2 << " is " << add(num1, num2) << std::endl;
+    std::cout << "The difference of " << num1 << " and " << num2 << " is " << subtract(num1, num2) << std::endl;
+    std::cout << "The product of " << num1 << " and " << num2 << " is " << multiply(num1, num2) << std::endl;
+    std::cout << "The division of " << num1 << " and " << num2 << " is " << divide(num1, num2) << std::endl;
+    std::cout << "The remainder of " << num1 << " and " << num2 << " is " << modulo(num1, num2) << std::endl;
 
     return 0;
 }
